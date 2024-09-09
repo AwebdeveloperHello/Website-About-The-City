265ce7692d5d328a88f02a05136c6b74b4d7e62f
AwebdeveloperHello <oleandrtur@gmail.com>
2024-09-09 19:36:54 +0300

    I've started the project
    The project about interesting places in Moscow.

---
+++ script.js
@@ -0,0 +1,32 @@
+function updateClock() {
+    
+    
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}`; 
+    document.getElementById("clock").textContent = timeString;
+
+}
+
+updateClock();
+setInterval(updateClock, 10);
+
+
+const button = document.getElementById('ChangeBack');
+const body = document.getElementById('BackGroundMain');  
+const storedColor = localStorage.getItem('backgroundColor');
+
+    
+if (storedColor) {
+    body.style.backgroundColor = storedColor;
+}
+
+button.addEventListener('click', () => {
+    const currentColor = body.style.backgroundColor;
+    const newColor = currentColor === 'white' ? 'rgb(20, 20, 20)' : 'white';
+
+    body.style.backgroundColor = newColor;
+    localStorage.setItem('backgroundColor', newColor); // Save the new color
+});
