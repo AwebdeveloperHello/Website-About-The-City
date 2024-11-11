26a6e16a80a2725aca703d15b3f44af641c01958
AwebdeveloperHello <oleandrtur@gmail.com>
2024-11-11 14:25:01 +0300

    Other signs were erased

--- Scripts/script.js
+++ Scripts/script.js
@@ -93,8 +93,8 @@ const jsFrame = new JSFrame();
 const frame = jsFrame.create({
     title: 'Оптимизация',
     left: 20, top: 20, width: 320, height: 220,
-    movable: true,//Enable to be moved by mouse
-    resizable: true,//Enable to be resized by mouse
+    movable: true,
+    resizable: true,
     html: '<div id="my_element" style="padding:10px;font-size:12px;color:darkgray;">Если сайт выглядит плохо/некорректно, используйте сочитание клавиш Ctrl+Shift+ -/+, чтобы менять размер экрана.</div>',
 });
 
